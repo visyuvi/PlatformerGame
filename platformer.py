92846b04ee1ce0e7d1223d080316f9eb082539aa
Your Name <you@example.com>
2023-07-28 21:32:04 +0530

    Added code for collision detection

--- platformer.py
+++ platformer.py
@@ -33,7 +33,7 @@ class Player:
 
         self.index = 0
         self.counter = 0
-        for num in range(1,5):
+        for num in range(1, 5):
             img_right = pygame.image.load(f'img/guy{num}.png')
             img_right = pygame.transform.scale(img_right, (40, 80))
             img_left = pygame.transform.flip(img_right, True, False)
@@ -44,6 +44,8 @@ class Player:
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.jumped = False
         self.direction = 0
@@ -96,7 +98,23 @@ class Player:
         if self.vel_y > 10:
             self.vel_y = 10
         dy += self.vel_y
+
         # check for collision
+        for tile in world.tile_list:
+            # check for collision in x direction
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0  # just don't move at all
+
+            # check for collision in y direction
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check if below the ground i.e., jumping
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                # check if above the ground i.e., falling
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
 
         # update player coordinates
         self.rect.x += dx
@@ -107,6 +125,7 @@ class Player:
             dy = 0
 
         screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
 
 class World:
@@ -138,6 +157,7 @@ class World:
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 world_data = [
