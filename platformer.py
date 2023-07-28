75eb7344fb77739615b04e54738f4dd7ad781508
Your Name <you@example.com>
2023-07-28 13:30:03 +0530

    Player Sprite Walking Animation added

--- platformer.py
+++ platformer.py
@@ -2,6 +2,9 @@ import pygame
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 screen_width = 1000
 screen_height = 1000
 
@@ -25,29 +28,69 @@ bg_img = pygame.image.load('img/sky.png')
 
 class Player:
     def __init__(self, x, y):
-        img = pygame.image.load('img/guy1.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = []
+        self.images_left = []
+
+        self.index = 0
+        self.counter = 0
+        for num in range(1,5):
+            img_right = pygame.image.load(f'img/guy{num}.png')
+            img_right = pygame.transform.scale(img_right, (40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
     def update(self):
 
         dx = 0
         dy = 0
+        walk_cooldown = 5
+
         # get key presses
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
             dx -= 5
+            self.counter += 1
+            self.direction = -1
+
         if key[pygame.K_RIGHT]:
             dx += 5
+            self.counter += 1
+            self.direction = 1
+
+        if not key[pygame.K_RIGHT] and not key[pygame.K_LEFT]:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            elif self.direction == -1:
+                self.image = self.images_left[self.index]
+
         if key[pygame.K_SPACE] and not self.jumped:
             self.vel_y = -15
             self.jumped = True
         if not key[pygame.K_SPACE]:
             self.jumped = False
+
+        # handle animation
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index == len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
         # add gravity
         self.vel_y += 1
         if self.vel_y > 10:
@@ -126,6 +169,7 @@ player = Player(100, screen_height - 130)
 run = True
 while run:
 
+    clock.tick(fps)
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
