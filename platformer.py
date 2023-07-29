bf9ff7e3d70d707cc8515fcef3904cac4e2d607a
Your Name <you@example.com>
2023-07-29 10:35:00 +0530

    Added enemies. They can oscillate to and fro in the horizontal direction

--- platformer.py
+++ platformer.py
@@ -122,7 +122,6 @@ class Player:
 
         if self.rect.bottom > screen_height:
             self.rect.bottom = screen_height
-            dy = 0
 
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
@@ -153,11 +152,32 @@ class World:
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+                    blob_group.add(blob)
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
             pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/blob.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_counter = 0
+        self.move_direction = 1
+
+    def update(self) -> None:
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
 
 
 world_data = [
@@ -183,8 +203,10 @@ world_data = [
     [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-world = World(world_data)
 player = Player(100, screen_height - 130)
+blob_group = pygame.sprite.Group()
+
+world = World(world_data)
 
 run = True
 while run:
@@ -195,6 +217,8 @@ while run:
 
     # draw_grid()
     world.draw()
+    blob_group.draw(screen)
+    blob_group.update()
     player.update()
 
     for event in pygame.event.get():
